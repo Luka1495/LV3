8264bfb3c368bdf10e4d600acd0d41586f27e761
Luka1495 <62608325+Luka1495@users.noreply.github.com>
2020-04-24 15:12:14 +0200

    Add files via upload

---
+++ LV3/zadatak4LV3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak4LV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleNotification message = new ConsoleNotification("Luka Vukadin", "Snjeguljica", "Sedam patuljaka", DateTime.Now, Category.INFO, ConsoleColor.DarkYellow);
+            NotificationManager Graditelj = new NotificationManager();
+
+            Graditelj.Display(message);
+        }
+    }
+}
+
+
---
+++ LV3/zadatak5LV3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak5LV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationBuilder Builder = new NotificationBuilder();
+            Builder.SetAuthor("Luka Vukadin");
+            Builder.SetColor(ConsoleColor.DarkBlue);
+            Builder.SetLevel(Category.INFO);
+            Builder.SetText("nesta novo");
+            Builder.SetTitle("Naslov");
+            Builder.SetTime(DateTime.Now);
+            ConsoleNotification message = Builder.Build();
+            Builder.Display(message);
+        }
+    }
+}
